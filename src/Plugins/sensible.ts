50e8d4b7567005a61997093609f8ecffb23eac6b
RodriiiG <rodrigo.godoy2712@gmail.com>
2025-09-05 18:34:06 -0300

    Delete src/Plugins directory

--- src/Plugins/sensible.ts
+++
@@ -1,6 +0,0 @@
-import fastifySensible from "@fastify/sensible";
-import fastifyPlugin from "fastify-plugin";
-
-export default fastifyPlugin(async function sensiblePlugin(fastify) {
-  fastify.register(fastifySensible);
-});
